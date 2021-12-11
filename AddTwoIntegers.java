1ac24571da6b19998d8c8c34a94bf94560945232
lokesh <kottelokesh789@gmail.com>
2021-12-11 18:02:47 +0530

    ADDED: integers

---
+++ AddTwoIntegers.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class AddTwoIntegers {
+	public static void main(String[] args) {
+	    
+	    System.out.println("Enter two numbers");
+	    int first = 40;
+	    int second = 50;
+	    
+	    System.out.println(first + " " + second);
+
+	    // add two numbers
+	    int sum = first + second;
+	    System.out.println("The sum is: " + sum);
+	}
+
+}
---
+++ PrintAnInteger.java
@@ -0,0 +1,14 @@
+package sample;
+import java.util.Scanner;
+public class PrintAnInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);{
+        System.out.print("Enter a number: ");
+        int number = reader.nextInt();
+        System.out.println("You entered: " + number);
+		
+	}
+	}
+}
